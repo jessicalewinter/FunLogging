e52183b7d707f54b3d39a38c4c0615f2e903c276
Jessica Lewinter <jessicalewinter@gmail.com>
2023-04-20 22:20:19 -0300

    ADD: Create FunLogging SPM Package

---
+++ Sources/FunLogging/FunLogging.swift
@@ -0,0 +1,6 @@
+public struct FunLogging {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FunLoggingTests/FunLoggingTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FunLogging
+
+final class FunLoggingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FunLogging().text, "Hello, World!")
+    }
+}
